52c25f15f9081283c750bc615b06ae7eeafc0b8b
nguyen <nguyen@digital-launch.com>
2023-01-25 09:47:41 +0700

    Facade Pattern 2

--- facade/function.js
+++ facade/function.js
@@ -5,16 +5,36 @@ class Discount {
 }
 
 class ShippingFee{
-    calc(value){
-        return value + 9;
+    calc(){
+        return 9;
     }
 }
 
 // implement Facade design pattern
+class FacadePattern{
+    constructor(){
+        this.discount = new Discount();
+        this.shippingFee = new ShippingFee();
+    }
+    calc(price){
+        // Facade: logic COMBINED together
+        // Strategy: logic SEPERATED, listed in the actions list
+        price = this.discount.calc(price);
+        console.log(`Price with discount::`, price);
+        price += this.shippingFee.calc();
+        console.log(`Price with discount and shipping fee::`, price);
+
+        return price;
+    }
+}
 
 function buy(price){
     // process price logic by Facade design pattern
+    const facadePattern = new FacadePattern();
+    price = facadePattern.calc(price);
+
     return price;
 }
 
-buy(50)
+const price = buy(50)
+console.log(`Final price::`, price);
